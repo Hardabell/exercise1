cb5893bfa0d31e58f2c99d4f0ad885721f04259f
Hardabell <hardabell@gmail.com>
2018-12-12 15:22:13 +0700

    mindahin file

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample{
+	public static void main(String args[]){
+		System.out.println("This is just a sample java file for git");
+		System.out.println("we are hello world");
+	System.out.println("this second modification in java file");
+	}
+		}
